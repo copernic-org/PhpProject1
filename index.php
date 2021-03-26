0de34d2e5de51c2198d65959338dc28221da3a2f
Pep <Pep@192.168.20.219>
2021-03-26 18:11:56 +0100

    Added cookie array indexed by 'author' and combo in now populated from bd.

--- index.php
+++ index.php
@@ -16,10 +16,14 @@ and open the template in the editor.
         <link rel="stylesheet" href="css/mystyle.css" >
         <script src="js/my_functions.js"></script>
     </head>
-    <body onload="showEstadistiques(1)">
+    <body onload="showEstadistiques('Totes')">
         <?php
         include_once 'classes/Config.php';
+        include_once 'classes/DatabasePDO.php';
         $config = new Config("xml/config.xml");
+        $db = new DatabasePDO("localhost", "root", "admin", "m07uf3");
+        $db->connect();
+        $result = $db->selectDistinctModalitats();
         echo "<h5 align='right'>Avui és " . date("Y/m/d") . "</h5>";
         echo $config->getTitle();
         echo $config->getSubtitle();
@@ -28,17 +32,38 @@ and open the template in the editor.
             echo "<p>Hola, no ens veiem des de: " . $_COOKIE[$cookie_name] . "</p>";
         }
         $cookie_value = date("d/m/Y");
+        // expires s'expressa en segons 60 * 60 * 24 = 86400 segons en un dia
+        // domain "/" tot el domini
         setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+        setcookie("author[name]", "Pep", strtotime('+30 days'), "/");
+        //setcookie("author[age]", null, -1, "/");
+        setcookie("author[phone]", "456453347", strtotime('+30 days'), "/");
+        setcookie("author[email]", "email@email.cat", strtotime('+30 days'), "/");
         ?>
         <form>
             <label>Modalitats:</label>
             <select class="dropdown-content" name="modalitats" onchange="showEstadistiques(this.value)">
-                <option value="1">Totes</option>
-                <option value="2">Humà</option>
-                <option value="3">Màquina</option>
+                <option value="Totes">Totes</option>
+                <?php
+                while ($row = $result->fetch()) {
+                    $modalitat = $row['modalitat'];
+                    echo "<option value='$modalitat'>" . $modalitat . "</option>";
+                }
+                ?>
             </select>
         </form>
         <br>
         <div id="taula_estadistiques_id"></div>
+        <?php
+        echo "<p>";
+        if (isset($_COOKIE['author'])) {
+            foreach ($_COOKIE['author'] as $name => $value) {
+                $name = htmlspecialchars($name);
+                $value = htmlspecialchars($value);
+                echo "$value ";
+            }
+        }
+        echo "</p>"
+        ?>
     </body>
 </html>
